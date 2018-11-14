952765045f083e10fc2a77c386f1d679bb0471d3
Dhr_Brick <dengheran@aliyun.com>
2018-11-14 16:16:52 +0800

    finish singer data

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,14 +1,87 @@
 <template>
-  <div>
+  <div class="singer">
     歌手
   </div>
 </template>
 
 <script>
-export default {}
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
+import Singer from 'common/js/singer'
+
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
+
+export default {
+  data() {
+    return {
+      singerList: []
+    }
+  },
+  created() {
+    this._getSingerList()
+    console.log(this.singerList)
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singerList = res.data.list
+          console.log(this._normalizeSize(this.singerList))
+        }
+      })
+    },
+    _normalizeSize(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_id,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: item.Findex,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_id,
+          name: item.Fsinger_name
+        }))
+      })
+      // 为了得到有序列表，我们需要处理 map
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  }
+}
 </script>
 
-<style lang="stylus" scoped rel="stylesheet/stylus">
-  @import '~common/stylus/variable'
-  @import '~common/stylus/mixin'
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
