178f994be43c5fb58c5b1e36a5881aba34b6f3f6
Dhr_Brick <dengheran@aliyun.com>
2019-01-12 17:08:48 +0800

    finish hotkey

--- src/base/search-box/search-box.vue
+++ src/base/search-box/search-box.vue
@@ -22,6 +22,9 @@ export default {
   methods: {
     clear() {
       this.query = ''
+    },
+    setQuery(query) {
+      this.query = query
     }
   },
   created() {
@@ -33,6 +36,8 @@ export default {
 </script>
 
 <style lang="stylus" scoped rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
   .search-box
     display: flex
     align-items: center
