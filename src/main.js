6685413d5c63b6b75049e62fd197ddc858d744e6
Dhr_Brick <dengheran@aliyun.com>
2019-03-07 20:52:19 +0800

    add errorHandler in main.js

--- src/main.js
+++ src/main.js
@@ -14,6 +14,13 @@ Vue.use(VueLazyload, {
   loading: require('common/image/default.png')
 })
 
+// 错误监控
+Vue.config.errorHandler = function (err, vm, info) {
+  console.log(err)
+  console.log(vm)
+  console.log(info)
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
